cdf218307bfcb12b0234a00bb6073afc0dd10494
EnguerranCobert <e.cobert@student.isartdigital.com>
2020-03-17 14:27:10 +0100

    init unity

---
+++ Assets/_Common/Scripts/Utils/ScreenShake.cs
@@ -0,0 +1,19 @@
+///-----------------------------------------------------------------
+/// Author : Enguerran COBERT
+/// Date : 05/12/2019 13:50
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.DefaultCompany.ToolsFab.FabTools {
+	public class ScreenShake : MonoBehaviour {
+	
+		private void Start () {
+			
+		}
+		
+		private void Update () {
+			
+		}
+	}
+}
